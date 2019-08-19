f1b695fcade58127bc76a0f73b5a6d7008282fc8
Constantin Musteata <costin.musteata@gmail.com>
2019-08-18 20:55:58 -0400

    BASE-4 improved functionality and readability

--- intro/main.cpp
+++ intro/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <set>
+#include <chrono>
+#include <ctime>
+#include <cassert>
 
 struct BRIDGE { int length; std::vector<double> paces; };
 const std::vector<BRIDGE> yaml = 
@@ -13,117 +15,187 @@ const std::vector<BRIDGE> yaml =
 
 struct Bridge
 {
+    int length;
+    double time;
+ 
+    Bridge()
+    : length(0)
+    , time(0.0)
+    {
+    }
+
     Bridge(int len)
     : length(len)
     , time(0.0)
     {
     }
-    int length;
-    double time;
-};
 
-void show(std::vector<std::vector<double>> groups, std::vector<Bridge> bridges)
-{
-    for (size_t k = 0; k < groups.size(); ++k)
+    const Bridge& operator +=(const Bridge& bridge)
     {
-        for (double pace : groups[k])
-        {
-            std::cout << pace << ",";
-        }
-        if (k < bridges.size())
-            std::cout << " [" << bridges[k].length << " ft, " << bridges[k].time << " min] ";
+        length += bridge.length;
+        time   += bridge.time;
+        return *this;
     }
-    std::cout << std::endl;
+
+    friend Bridge operator +(const Bridge& lhs, const Bridge& rhs)
+    {
+        Bridge bridge;
+        bridge.length = lhs.length + rhs.length;
+        bridge.time = lhs.time + rhs.time;
+        return bridge;
+    }
+
+    friend std::ostream& operator <<(std::ostream& os, const Bridge& bridge)
+    {
+        os << "[" << bridge.length << " ft, " << bridge.time << " min]";
+        return os;
+    }
 };
 
-int main()
+class Algo
 {
+    Bridge result;
     std::vector<Bridge> bridges;
     std::vector<std::vector<double>> groups;
-    for (const auto& y : yaml)
-    {
-        bridges.emplace_back(y.length);
-        groups.emplace_back();
-        auto& group = groups.back();
-        for (double pace : y.paces)
-            group.push_back(pace);
-    }
-    groups.emplace_back();
 
-    for (size_t k = 0; k < bridges.size(); ++k)
+    void show(std::vector<std::vector<double>> groups, std::vector<Bridge> bridges)
     {
-        std::sort(groups[k].begin(), groups[k].end(), std::greater<double>());
-        show(groups, bridges);
-
-        auto& bridge = bridges[k];
-        for (size_t n = groups[k].size(); n > 0; n = groups[k].size())
+        for (size_t k = 0; k < groups.size(); ++k)
         {
-            if (n == 1)
-            {
-                // one-way trip
-                // timing for hiker [0]
-                bridge.time += bridge.length / groups[k][0];
-
-                // crossing for the last hiker
-                groups[k+1].push_back(groups[k][0]);
-                groups[k].pop_back();
-            }
-            else if (n == 2)
-            {
-                // one-way trip
-                // timing for hikers [0] and [1]
-                bridge.time += bridge.length / groups[k][1];
-
-                // crossing for the last 2 hikers
-                groups[k+1].push_back(groups[k][1]);
-                groups[k+1].push_back(groups[k][0]);
-                groups[k].pop_back();
-                groups[k].pop_back();
-            }
-            else if (n == 3)
-            {
-                // first round-trip
-                // timing for the fastest pair of hikers [0] and [1] ([0] crosses back)
-                bridge.time += bridge.length / groups[k][1];
-                bridge.time += bridge.length / groups[k][0];
-
-                // second one-way trip
-                // timing for the slowest pair of hikers [2] and [0]
-                bridge.time += bridge.length / groups[k][2];
-
-                // transfer for the last 3 hikers
-                groups[k+1].push_back(groups[k][2]);
-                groups[k+1].push_back(groups[k][1]);
-                groups[k+1].push_back(groups[k][0]);
-                groups[k].pop_back();
-                groups[k].pop_back();
-                groups[k].pop_back();
-            }
-            else
-            {
-                // first round-trip
-                // timing for the fastest pair of hikers [0] and [1] ([0] crosses back)
-                bridge.time += bridge.length / groups[k][1];
-                bridge.time += bridge.length / groups[k][0];
-
-                // second round-trip
-                // timing for the slowest pair of hikers [j] and [j-1] ([1] crosses back)
-                size_t j = n - 1;
-                bridge.time += bridge.length / groups[k][j];
-                bridge.time += bridge.length / groups[k][1];
-
-                // transfer the slowest pair of hikers
-                groups[k+1].push_back(groups[k][j-1]);
-                groups[k+1].push_back(groups[k][j]);
-                groups[k].pop_back();
-                groups[k].pop_back();
-            }
-
-            // display the progress as we transfer the hikers
-            show(groups, bridges);
+            for (double pace : groups[k])
+                std::cerr << pace << ',';
+            if (k < bridges.size())
+                std::cerr << bridges[k] << ' ';
         }
+        std::cerr << std::endl;
     }
 
+public:
+    Algo(const std::vector<BRIDGE>& yaml)
+    {
+        for (const auto& y : yaml)
+        {
+            bridges.emplace_back(y.length);
+            groups.emplace_back();
+            auto& group = groups.back();
+            for (double pace : y.paces)
+                group.push_back(pace);
+        }
+        groups.emplace_back();
+    }
+
+    void run()
+    {
+        Bridge total;
+        for (size_t k = 0; k < bridges.size(); ++k)
+        {
+            std::sort(groups[k].begin(), groups[k].end(), std::greater<double>());
+            show(groups, bridges);
+
+            auto& bridge = bridges[k];
+            for (size_t n = groups[k].size(); n > 0; n = groups[k].size())
+            {
+                if (n == 1)
+                {
+                    // one-way trip
+                    // timing for hiker [0]
+                    bridge.time += bridge.length / groups[k][0];
+
+                    // crossing for the last hiker
+                    groups[k+1].push_back(groups[k][0]);
+                    groups[k].pop_back();
+                }
+                else if (n == 2)
+                {
+                    // one-way trip
+                    // timing for hikers [0] and [1]
+                    bridge.time += bridge.length / groups[k][1];
+
+                    // crossing for the last 2 hikers
+                    groups[k+1].push_back(groups[k][1]);
+                    groups[k+1].push_back(groups[k][0]);
+                    groups[k].pop_back();
+                    groups[k].pop_back();
+                }
+                else if (n == 3)
+                {
+                    // first round-trip
+                    // timing for the fastest pair of hikers [0] and [1] ([0] crosses back)
+                    bridge.time += bridge.length / groups[k][1];
+                    bridge.time += bridge.length / groups[k][0];
+
+                    // second one-way trip
+                    // timing for the slowest pair of hikers [2] and [0]
+                    bridge.time += bridge.length / groups[k][2];
+
+                    // transfer for the last 3 hikers
+                    groups[k+1].push_back(groups[k][2]);
+                    groups[k+1].push_back(groups[k][1]);
+                    groups[k+1].push_back(groups[k][0]);
+                    groups[k].pop_back();
+                    groups[k].pop_back();
+                    groups[k].pop_back();
+                }
+                else
+                {
+                    // first round-trip
+                    // timing for the fastest pair of hikers [0] and [1] ([0] crosses back)
+                    bridge.time += bridge.length / groups[k][1];
+                    bridge.time += bridge.length / groups[k][0];
+
+                    // second round-trip
+                    // timing for the slowest pair of hikers [j] and [j-1] ([1] crosses back)
+                    size_t j = n - 1;
+                    bridge.time += bridge.length / groups[k][j];
+                    bridge.time += bridge.length / groups[k][1];
+
+                    // transfer the slowest pair of hikers
+                    groups[k+1].push_back(groups[k][j-1]);
+                    groups[k+1].push_back(groups[k][j]);
+                    groups[k].pop_back();
+                    groups[k].pop_back();
+                }
+
+                // display the progress as we transfer the hikers
+                show(groups, bridges);
+            }
+
+            // total crossing all bridges
+            total += bridge;
+        }
+
+        result = total;
+    }
+
+    friend std::ostream& operator <<(std::ostream& os, const Algo& algo)
+    {
+        os << algo.result;
+        return os;
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    assert(argc == 2);
+    std::string filename = argv[1];
+
+    std::cout << "filename: " << filename << std::endl;
+
+    auto begin = std::chrono::system_clock::now();
+    time_t begin_time = std::chrono::system_clock::to_time_t(begin);
+
+    Algo algo(yaml);
+    algo.run();
+
+    std::cout << "result: " << algo << std::endl;
+
+    auto end = std::chrono::system_clock::now();
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+    std::chrono::duration<double> elapsed_seconds = end - begin;
+
+    std::cout << "begin time: " << std::ctime(&begin_time);
+    std::cout << "  end time: " << std::ctime(&end_time);
+    std::cout << "   elapsed: " << elapsed_seconds.count() << " seconds"  << std::endl;
     return 0;
 }
 
--- intro/run.sh
+++ intro/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 BEGIN=$(date +%s)
-./intro 2> /dev/null
+./intro intro.yaml 2> /dev/null
 END=$(date +%s)
 echo "elapsed "$((END-BEGIN))" seconds"
