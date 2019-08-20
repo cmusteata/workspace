ef5a4514fd885e935f4f0334b1d02e475f103437
Constantin Musteata <costin.musteata@gmail.com>
2019-08-20 04:16:06 -0400

    BASE-5 with Yaml parser and more comments

--- intro/main.cpp
+++ intro/main.cpp
@@ -4,14 +4,127 @@
 #include <chrono>
 #include <ctime>
 #include <cassert>
+#include <fstream>
 
-struct BRIDGE { int length; std::vector<double> paces; };
-const std::vector<BRIDGE> yaml = 
+// struct BRIDGE
+//
+// This data-structure is used
+// internally by the Yaml parser
+
+struct BRIDGE
 {
-    { 100, {100, 50, 20, 10}},
-    { 250, {2.5}},
-    { 150, {25,15}}
+    int length;
+    std::vector<double> paces;
 };
+
+// class Yaml
+//
+// This class is not intended to parse any generic .yaml file
+// The sole purpose of this class is to parse the intro.yaml
+//
+// The following assumptions are made in order to parse:
+// 1) The intro.yaml file is a valid yaml formatted file
+// 2) All comments start at index 0
+// 3) The sequence indicator is "- "
+// 4) The keyvalue indicator is ": "
+// 5) All sequences in a block are aligned relative to the same column
+// 6) The underlying data structure of 'Yaml' is a sequence of BRIDGES
+//    BRIDGE entries in the intro.yaml file start at index 0
+// 7) Each BRIDGE has two key-value pairs starting at index 2
+//    length: scalar-value
+//    paces: list of scalar-values
+
+class Yaml
+{
+    std::string fname;
+    std::ifstream ifs;
+    std::vector<BRIDGE> data;
+
+    void parse(const std::string& line)
+    {
+        // ignore all comments
+        if (line.find("# ") == 0)
+            return;
+        
+        // looking for a sequence indicator
+        std::size_t pos = line.find("- ");
+        if (pos != std::string::npos)
+        {
+            if (pos == 0)
+            {
+                // emplace an empty  BRIDGE entry
+                data.emplace_back();
+                
+                std::cerr << "[" << pos << "] " << "- " << std::endl;
+            }
+            else
+            {
+                assert(data.empty() == false);
+
+                // emplace a new hiking-pace for this BRIDGE
+                std::string value = line.substr(pos += 2);
+                data.back().paces.emplace_back(std::stod(value));
+
+                std::cerr << "[" << pos << "] " << "  - " << value << std::endl;
+            }
+        }
+
+        // looking for a key-value indicator
+        pos = line.find(": ");
+        if (pos != std::string::npos)
+        {
+            const std::string key1 = "length";
+            const std::string key2 = "paces";
+
+            if (line.find(key1, 2) != std::string::npos)
+            {
+                assert(data.empty() == false);
+
+                // sent the length of the bridge
+                std::string value = line.substr(pos += 2);
+                data.back().length = std::stoi(value);
+
+                std::cerr << "[" << pos << "]   " << key1 << ": " << value << std::endl;
+            }
+            else if (line.find(key2, 2) != std::string::npos)
+            {
+                std::cerr << "[" << pos << "]   " << key2 << ": " << std::endl;
+            }
+        }
+    }
+
+public:
+    Yaml(const std::string& filename)
+    : fname(filename)
+    , ifs(filename)
+    {
+    }
+
+    ~Yaml()
+    {
+        ifs.close();
+    }
+    
+    void parse()
+    {
+        assert(ifs.is_open() == true);
+
+        std::string line;
+        getline(ifs, line);
+        assert(line == "---");
+
+        while (getline(ifs, line))
+            parse(line);
+    }
+
+    const std::vector<BRIDGE>& get() const
+    {
+        return data;
+    }
+};
+
+// Data structure used internally by the 'class Algo'
+// to compute the crossing distance & the crosing time
 
 struct Bridge
 {
@@ -37,14 +150,6 @@ struct Bridge
         return *this;
     }
 
-    friend Bridge operator +(const Bridge& lhs, const Bridge& rhs)
-    {
-        Bridge bridge;
-        bridge.length = lhs.length + rhs.length;
-        bridge.time = lhs.time + rhs.time;
-        return bridge;
-    }
-
     friend std::ostream& operator <<(std::ostream& os, const Bridge& bridge)
     {
         os << "[" << bridge.length << " ft, " << bridge.time << " min]";
@@ -52,13 +157,19 @@ struct Bridge
     }
 };
 
+// This Algo computes the crossing time and total distance
+// The assumption is made that a faster hiker in the [k + 1] group
+// can't help the hikers in [k] group to improve the crossing time
+// Each group [k] independently has to cross the bridge and then
+// join the [n+1] group
+
 class Algo
 {
     Bridge result;
     std::vector<Bridge> bridges;
     std::vector<std::vector<double>> groups;
 
-    void show(std::vector<std::vector<double>> groups, std::vector<Bridge> bridges)
+    void show() const
     {
         for (size_t k = 0; k < groups.size(); ++k)
         {
@@ -71,9 +182,9 @@ class Algo
     }
 
 public:
-    Algo(const std::vector<BRIDGE>& yaml)
+    Algo(const Yaml& yaml)
     {
-        for (const auto& y : yaml)
+        for (const auto& y : yaml.get())
         {
             bridges.emplace_back(y.length);
             groups.emplace_back();
@@ -90,7 +201,7 @@ public:
         for (size_t k = 0; k < bridges.size(); ++k)
         {
             std::sort(groups[k].begin(), groups[k].end(), std::greater<double>());
-            show(groups, bridges);
+            show();
 
             auto& bridge = bridges[k];
             for (size_t n = groups[k].size(); n > 0; n = groups[k].size())
@@ -101,7 +212,7 @@ public:
                     // timing for hiker [0]
                     bridge.time += bridge.length / groups[k][0];
 
-                    // crossing for the last hiker
+                    // transfer for the last hiker
                     groups[k+1].push_back(groups[k][0]);
                     groups[k].pop_back();
                 }
@@ -111,7 +222,7 @@ public:
                     // timing for hikers [0] and [1]
                     bridge.time += bridge.length / groups[k][1];
 
-                    // crossing for the last 2 hikers
+                    // transfer for the last 2 hikers
                     groups[k+1].push_back(groups[k][1]);
                     groups[k+1].push_back(groups[k][0]);
                     groups[k].pop_back();
@@ -157,7 +268,7 @@ public:
                 }
 
                 // display the progress as we transfer the hikers
-                show(groups, bridges);
+                show();
             }
 
             // total crossing all bridges
@@ -169,7 +280,9 @@ public:
 
     friend std::ostream& operator <<(std::ostream& os, const Algo& algo)
     {
-        os << algo.result;
+        for (size_t k = 0; k < algo.bridges.size(); ++k)
+            os << "bridge " << k << ": " << algo.bridges[k] << std::endl;
+        os << "overall : " << algo.result;
         return os;
     }
 };
@@ -179,15 +292,18 @@ int main(int argc, char* argv[])
     assert(argc == 2);
     std::string filename = argv[1];
 
-    std::cout << "filename: " << filename << std::endl;
+    std::cout << "open file: " << filename << std::endl;
 
     auto begin = std::chrono::system_clock::now();
     time_t begin_time = std::chrono::system_clock::to_time_t(begin);
 
+    Yaml yaml(filename);
+    yaml.parse();
+
     Algo algo(yaml);
     algo.run();
 
-    std::cout << "result: " << algo << std::endl;
+    std::cout << algo << std::endl;
 
     auto end = std::chrono::system_clock::now();
     std::time_t end_time = std::chrono::system_clock::to_time_t(end);
