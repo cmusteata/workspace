7d860cc1613091472e2c897c4a6f4c62fb75399a
Constantin Musteata <costin.musteata@gmail.com>
2019-09-13 14:29:55 -0400

    BASE-16 cirque prototype push front/back

--- cirque/ring_buffer.cpp
+++ cirque/ring_buffer.cpp
@@ -4,7 +4,9 @@ ring_buffer::ring_buffer()
 : buffer(nullptr)
 , rows(1000)
 , cols(256)
-{
+, i(0)
+, j(256)
+{    
     buffer = static_cast<char*>(operator new(rows * cols));
 }
 
@@ -12,6 +14,8 @@ ring_buffer::ring_buffer(int num_rows, int num_cols)
 : buffer(nullptr)
 , rows(num_rows)
 , cols(num_cols)
+, i(0)
+, j(num_cols)
 {    
     buffer = static_cast<char*>(operator new(rows * cols));
 }
@@ -19,4 +23,41 @@ ring_buffer::ring_buffer(int num_rows, int num_cols)
 ring_buffer::~ring_buffer()
 {
     operator delete(buffer);
+}
+
+inline 
+int ring_buffer::next(int index) const
+{
+    int n = index + cols;
+    return n < rows * cols ? n : 0;
+}
+
+inline
+bool ring_buffer::empty() const
+{
+    return next(i) == j;
+}
+
+inline
+char* ring_buffer::front()
+{
+    return &buffer[i];
+}
+
+inline
+char* ring_buffer::back()
+{
+    return &buffer[j];
+}
+
+inline
+void ring_buffer::push_front()
+{
+    ++i;
+}
+
+inline
+void ring_buffer::push_back()
+{
+    ++j;
 }
--- cirque/ring_buffer.h
+++ cirque/ring_buffer.h
@@ -1,10 +1,21 @@
-class ring_buffer
+#ifndef __RING_BUFFER_H__
+#define __RING_BUFFER_H__
+
+class ring_buffer final
 {
     char* buffer;
-    int rows;
-    int cols;
+    int rows, cols;
+    int i, j;
+    int next(int index) const;
 public:
     ring_buffer();
     ring_buffer(int num_rows, int num_cols);
-    virtual ~ring_buffer();
+    ~ring_buffer();
+    bool empty() const;
+    char* front();
+    char* back();
+    void push_front();
+    void push_back();
 };
+
+#endif // __RING_BUFFER_H__
